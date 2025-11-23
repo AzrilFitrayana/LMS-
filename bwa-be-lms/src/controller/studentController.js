4620baa7169556d643e1660068a11a8595a575e0
azrilFitrayana <azrilfitrayana77@gmail.com>
2025-11-23 22:22:37 +0700

    be create API post students

--- bwa-be-lms/src/controller/studentController.js
+++ bwa-be-lms/src/controller/studentController.js
@@ -1,4 +1,7 @@
+import fs from "fs";
+import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
+import { mutateStudentSchema } from "../utils/schema.js";
 
 export const getStudent = async (req, res) => {
   try {
@@ -10,6 +13,50 @@ export const getStudent = async (req, res) => {
     return res.status(200).json({
       message: "Get Student Success",
       data: students,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const postStudent = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const parse = mutateStudentSchema.safeParse(body);
+
+    if (!parse.success) {
+      const errorMessage = parse.error.issues.map((err) => err.message);
+
+      // hapus gambar
+      if (req?.file?.path && fs.existsSync(req?.file?.path)) {
+        fs.unlinkSync(req.file.path);
+      }
+
+      return res.status(400).json({
+        message: "Error Validation",
+        data: null,
+        errors: errorMessage,
+      });
+    }
+
+    const hashPassword = await bcrypt.hash(body.password, 12);
+
+    const student = new userModel({
+      name: parse.data.name,
+      email: parse.data.email,
+      password: hashPassword,
+      foto: req.file?.filename,
+      manager: req.user?._id,
+      role: "student",
+    });
+
+    const response = await student.save();
+
+    return res.status(200).json({
+      message: "Post student success",
+      data: response,
     });
   } catch (error) {
     console.log(error);
--- bwa-be-lms/src/routes/studentRoutes.js
+++ bwa-be-lms/src/routes/studentRoutes.js
@@ -1,9 +1,22 @@
 import express from "express";
+import multer from "multer";
+import { fileFilter } from "../utils/multer.js";
+import { fileStorage } from "../utils/multer.js";
 import { verifyToken } from "../middleware/verifyToken.js";
-import { getStudent } from "../controller/studentController.js";
+import { getStudent, postStudent } from "../controller/studentController.js";
 
 const studentRoutes = express.Router();
+const upload = multer({
+  storage: fileStorage("students"),
+  fileFilter,
+});
 
 studentRoutes.get("/students", verifyToken, getStudent);
+studentRoutes.post(
+  "/students",
+  verifyToken,
+  upload.single("avatar"),
+  postStudent
+);
 
 export default studentRoutes;
